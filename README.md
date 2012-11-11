e962c92743e80c4ac3ef6011bd1f6fbe9e9cf96a
Peter Ruibal <ruibalp@gmail.com>
2012-11-11 02:42:47 -0800

    setup.py beefiness.  Bump to 0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 facebook-dbus is a reference implementation of a [sparts](http://github.com/fmoo/sparts)
 service that exports Facebook's graph API over dbus.
+
+Dependencies
+============
+facebook-dbus depends on a smattering of unusually diverse python libraries:
+
+ * keyring
+ * sparts (which depends on crazy stuff, like thrift)
+ * dbus-python (which depends on python-gobject)
+ * Twisted-web
--- setup.py
+++ setup.py
@@ -1,8 +1,32 @@
-import os
 from setuptools import setup, find_packages
+from distutils.spawn import find_executable
+import os.path
+import imp
+import pandoc.core
+
+def require_binary(name):
+    path = find_executable(name)
+    assert path is not None, \
+        "'%s' is a required binary for building sparts.\n" \
+        "Please install it somewhere in your PATH to run this command." \
+        % (name)
+    return path
+
+
+pandoc.core.PANDOC_PATH = require_binary('pandoc')
+ROOT = os.path.abspath(os.path.dirname(__file__))
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(os.path.join(ROOT, fname)).read()
+
+def read_md_as_rest(fname):
+    doc = pandoc.Document()
+    doc.markdown = read(fname)
+    return doc.rst
+
+def version():
+    file, pathname, description = imp.find_module('fb_dbus', [ROOT])
+    return imp.load_module('fb_dbus', file, pathname, description).__version__
 
 setup(
     name = "fb_dbus",
@@ -15,7 +39,7 @@ setup(
     keywords = "facebook sparts service daemon dbus",
     url = "http://github.com/fmoo/fb_dbus",
     packages = find_packages(),
-    long_description=read('README'),
+    long_description=read_md_as_rest("README.md"),
     install_requires=['keyring',
                       'sparts',
                       #'dbus-python',  # dbus-python does not have setup.py :(
